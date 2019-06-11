33b57164ae5a7c4cf6ff8009a23c5534eb5718e4
pjy4617 <jypark.t1@gmail.com>
2019-06-11 23:50:04 +0900

    openCV4.0 테스트

---
+++ StudyOpenCV/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace StudyOpenCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Mat src = new Mat("../../lenna.png", ImreadModes.Grayscale);
+            Mat dst = new Mat();
+            Cv2.Canny(src, dst, 50, 200);
+            using(new Window("src image", src))
+            using(new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }
+        }
+    }
+}
